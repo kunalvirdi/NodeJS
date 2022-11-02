993afaa9d543d9102cae66247fc5d18637479bb9
kunalvirdi <kunalvirdi.patiala@gmail.com>
2022-11-02 15:34:57 +0530

    Changes

--- practice/models/product.js
+++ practice/models/product.js
@@ -1,32 +1,17 @@
-const db = require('../util/database');
+const Sequelize=require('sequelize')
 
-const Cart = require('./cart');
+const sequelize=require('../util/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Product=sequelize.define('product',{
+  id:{
+    type:Sequelize.INTEGER,
+    autocomplete:true,
+    allowNull:false,
+    primaryKey:true
+  },
+  title:Sequelize.STRING,
+  price:Sequelize.DOUBLE,
+  description:Sequelize.STRING,
+  imageUrl:Sequelize.STRING
+})
 
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {
-
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
--- practice/util/database.js
+++ practice/util/database.js
@@ -1,10 +1,16 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+//
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node_complete',
+//     password: 'root@123'
+// });
+//
+// module.exports = pool.promise();
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: 'root@123'
-});
+const Sequelize=require('sequelize')
 
-module.exports = pool.promise();
+const sequelize=new Sequelize('node-complete','root','root@123',{dialect:'mysql',host:'localhost'});
+
+module.exports=sequelize;

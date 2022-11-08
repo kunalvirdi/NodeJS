05ae9a34a093fd46988026eab6d1eed9c54b7f64
kunalvirdi <kunalvirdi.patiala@gmail.com>
2022-11-08 13:33:03 +0530

    Changes

--- practice/util/database.js
+++ practice/util/database.js
@@ -1,16 +1,8 @@
-// const mysql = require('mysql2');
-//
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'node_complete',
-//     password: 'root@123'
-// });
-//
-// module.exports = pool.promise();
+const Sequelize = require('sequelize');
 
-const Sequelize=require('sequelize')
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
-const sequelize=new Sequelize('node-complete','root','root@123',{dialect:'mysql',host:'localhost'});
-
-module.exports=sequelize;
+module.exports = sequelize;
